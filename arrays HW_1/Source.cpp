53d46d5ad9ab35fd6c554c55930aa84281a87bc8
айкеновад <@229_13>
2018-07-04 08:27:30 +0600

    Add project files.

---
+++ arrays HW_1/Source.cpp
@@ -0,0 +1,94 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+#include<math.h>
+#include<time.h>
+#include <Windows.h>
+#include <iostream>
+#include <iomanip> 
+
+using namespace std;
+
+int main()
+{
+	srand(time(NULL));
+
+	setlocale(LC_ALL, "Rus");
+
+	int tn = 0;
+
+start:
+
+	cout << "Задача № ";
+	cin >> tn;
+
+	switch (tn)
+	{
+	case 1:
+	{
+
+	}
+	break;
+	case 2:
+	{
+
+	}
+	break;
+	case 3:
+	{
+
+	}
+	break;
+	case 4:
+	{
+
+	}
+	break;
+	case 5:
+	{
+
+	}
+	break;
+	case 6:
+	{
+
+	}
+	break;
+	case 7:
+	{
+
+	}
+	break;
+	case 8:
+	{
+
+	}
+	break;
+	case 9:
+	{
+
+	}
+	break;
+	case 10:
+	{
+
+	}
+	break;
+	case 11:
+	{
+
+	}
+	break;
+	case 12:
+	{
+
+	}
+	break;
+	default:
+		cout << "нет такой задачи" << endl << endl;
+	}
+	goto start;
+
+	system("pause");
+	return 0;
+}
